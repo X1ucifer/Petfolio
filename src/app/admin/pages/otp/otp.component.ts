b931a67456544c8aef9cdb2bdd217fcc6b542896
Madhan-vj <59563681+Madhan-vj@users.noreply.github.com>
2021-05-04 11:26:06 +0530

    otp changes

--- src/app/admin/pages/otp/otp.component.ts
+++ src/app/admin/pages/otp/otp.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 
 @Component({
   selector: 'app-otp',
@@ -11,5 +11,43 @@ export class OtpComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  otp: string;
+  showOtpComponent = true;
+  @ViewChild('ngOtpInput', { static: false}) ngOtpInput: any;
+  config = {
+    allowNumbersOnly: false,
+    length: 5,
+    isPasswordInput: false,
+    disableAutoFocus: false,
+    placeholder: '',
+    inputStyles: {
+      'width': '50px',
+      'height': '50px'
+    }
+  };
+  onOtpChange(otp) {
+    this.otp = otp;
+  }
 
+  setVal(val) {
+    this.ngOtpInput.setValue(val);
+  }
+
+  toggleDisable(){
+    if(this.ngOtpInput.otpForm){
+      if(this.ngOtpInput.otpForm.disabled){
+        this.ngOtpInput.otpForm.enable();
+      }else{
+        this.ngOtpInput.otpForm.disable();
+      }
+    }
+  }
+
+  onConfigChange() {
+    this.showOtpComponent = false;
+    this.otp = null;
+    setTimeout(() => {
+      this.showOtpComponent = true;
+    }, 0);
+  }
 }
